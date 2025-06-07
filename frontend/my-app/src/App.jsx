4ec300a00422946cb228846a8c48797fb905762c
Om Singh Mehta <ommehta912@gmail.com>
2025-06-07 19:46:34 +0530

    frontend setup , Navbar,login,signup created

--- frontend/my-app/src/App.jsx
+++ frontend/my-app/src/App.jsx
@@ -1,35 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
+import React from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Login from "./components/auth/Login.jsx";
+import Signup from "./components/auth/Signup.jsx";
+import Home from "./components/Home.jsx";
+import Job from "./components/Job.jsx";
+import Browse from "./components/Browse.jsx";
+import Profile from "./components/Profile.jsx";
+import JobDescription from "./components/JobDescription.jsx";
+import Companies from "./components/admin/Companies.jsx";
+import CompanyCreate from "./components/admin/CompanyCreate.jsx";
+import CompanySetUp from "./components/admin/CompanySetUp.jsx";
+import AdminJobs from "./components/admin/AdminJobs.jsx";
+import PostJob from "./components/admin/PostJob.jsx";
+import Applicants from "./components/admin/Applicants.jsx";
+import ProtectedRoute from "./components/admin/ProtectedRoute.jsx";
+import Bookmarks from "./components/Bookmarks.jsx";
+import JobSetUp from "./components/admin/JobSetUp.jsx";
+const appRouter = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+  },
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/signup",
+    element: <Signup />,
+  },
+  {
+    path: "/jobs",
+    element: <Job />,
+  },
+  {
+    path: "/browse",
+    element: <Browse />,
+  },
+  {
+    path: "/profile",
+    element: <Profile />,
+  },
+  {
+    path: "/description/:id",
+    element: <JobDescription />,
+  },
+  {
+    path: "/bookmarks",
+    element: <Bookmarks />,
+  },
+  // admin
+  {
+    path: "/admin/companies",
+    element: <ProtectedRoute><Companies /></ProtectedRoute>,
+  },
+  {
+    path: "/admin/companies/create",
+    element: <ProtectedRoute><CompanyCreate /></ProtectedRoute>,
+  },
+  {
+    path: "/admin/companies/:id",
+    element: <ProtectedRoute><CompanySetUp /></ProtectedRoute>,
+  },
+  {
+    path: "/admin/jobs/",
+    element: <ProtectedRoute><AdminJobs/></ProtectedRoute>,
+  },
+  {
+    path: "/admin/jobs/:id",
+    element: <ProtectedRoute><JobSetUp /></ProtectedRoute>,
+  },
+  {
+    path: "/admin/jobs/create",
+    element:<ProtectedRoute><PostJob /></ProtectedRoute> ,
+  },
+    {
+    path: "/admin/jobs/:id/applicants",
+    element: <ProtectedRoute><Applicants /></ProtectedRoute>,
+  },
+]);
+const App = () => {
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <RouterProvider router={appRouter} />
     </>
-  )
-}
-
-export default App
+  );
+};
+export default App;
