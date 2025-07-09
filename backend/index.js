436aff5b7aee6214eb503f4e323ec3fc33c18391
Om Singh Mehta <ommehta912@gmail.com>
2025-07-09 20:35:29 +0530

    URL updated

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 
 const corsOption={
-    origin:'http://localhost:5173',
+    origin:'https://j-portal.onrender.com',
     credentials:true
 }
 app.use(cors(corsOption));
